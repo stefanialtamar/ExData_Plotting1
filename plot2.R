dfc947ab4850149c2c744256e7a550c6468be6bc
stefanialtamar <68857310+stefanialtamar@users.noreply.github.com>
2020-09-22 20:54:49 -0500

    Add files via upload

---
+++ plot2.R
@@ -0,0 +1,8 @@
+library("data.table")
+powerconsumption <- data.table::fread(input = "household_power_consumption.txt")
+powerconsumption[, Global_active_power := lapply(.SD, as.numeric), .SDcols = c("Global_active_power")]
+powerconsumption[, dateTime := as.POSIXct(paste(Date, Time), format = "%d/%m/%Y %H:%M:%S")]
+powerconsumption <- powerconsumption[(dateTime >= "2007-02-01") & (dateTime < "2007-02-03")]
+png("plot2.png", width=480, height=480)
+plot(x = powerconsumption[, dateTime], y = powerconsumption[, Global_active_power], type="l",xlab="",ylab="Global Active Power(Kw)")
+dev.off()
